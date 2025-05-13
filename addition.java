dc4c3405eda47f1e5d7450d36fade0c4d09de596
bbk <babu.k.barrey@gmail.com>
2025-05-13 09:02:38 -0700

    first program file

---
+++ addition.java
@@ -0,0 +1,14 @@
+ Volume in drive C is Windows-SSD
+ Volume Serial Number is E409-4D21
+
+ Directory of C:\Users\babun\OneDrive\Desktop\CICD_Prj
+
+05/12/2025  02:28 PM    <DIR>          .
+05/12/2025  01:21 PM    <DIR>          ..
+05/12/2025  02:28 PM                 0 addition.java
+05/12/2025  02:17 PM               903 hello1.class
+05/12/2025  02:13 PM               201 hello1.java
+05/12/2025  02:17 PM               905 hello2.class
+05/12/2025  01:48 PM               203 hello2.java
+               5 File(s)          2,212 bytes
+               2 Dir(s)  10,405,175,296 bytes free
